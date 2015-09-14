3cd1b5c44aed384318341314fdd51227042d93fc
Guus Beckett <guus@reupload.nl>
2015-09-14 14:32:22 +0200

    Added initial project files

---
+++ Airports/Airports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airports
+//
+//  Created by Guus Beckett on 14/09/15.
+//  Copyright © 2015 Reupload. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
