fa7c1ce61a6a4fd77e5b6f1ce184b15678f34d93
Guus Beckett <guus@reupload.nl>
2015-09-14 14:49:40 +0200

    Added licence notice to source files and added airport class (swift file)

--- Airports/Airports/Airport.swift
+++ Airports/Airports/Airport.swift
@@ -1,6 +1,6 @@
 /*
 *
-* ViewController.swift
+* Airport.swift
 * Airports
 * Created by Guus Beckett on 14/09/15.
 *
@@ -18,20 +18,4 @@
 * See the Licence for the specific language governing permissions and limitations under the Licence.
 */
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
+import Foundation
